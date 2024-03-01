52fb4c90560e896949daa6b1e71689ec0b8231d7
mnlazs <mnl.azs@gmail.com>
2024-03-01 00:23:22 -0600

    updated

--- holbegram/lib/screens/Pages/bottom_nav.dart
+++ holbegram/lib/screens/Pages/bottom_nav.dart
@@ -7,7 +7,6 @@ import 'add_image.dart';
 import 'favorite.dart';
 import 'profile_screen.dart';
 
-
 class BottomNav extends StatefulWidget {
   const BottomNav({super.key});
   @override
@@ -42,14 +41,13 @@ class _BottomNavState extends State<BottomNav> {
       body: PageView(
         controller: _pageController,
         onPageChanged: onPageChanged,
-        children: [
-          const Feed(),
-          const Search(),
-          const AddImage(),
-          const Favorite(),
-          const ProfileScreen(),
+        children: const [
+          Feed(),
+          Search(),
+          AddImage(),
+          Favorite(),
+          ProfileScreen(),
         ],
-     
       ),
       bottomNavigationBar: BottomNavyBar(
         selectedIndex: _currentIndex,
@@ -57,41 +55,41 @@ class _BottomNavState extends State<BottomNav> {
         itemCornerRadius: 8,
         curve: Curves.easeInBack,
         onItemSelected: (index) {
-          _pageController.animateToPage(index, duration: Duration(milliseconds: 300), curve: Curves.ease);
+          _pageController.animateToPage(index, duration: const Duration(milliseconds: 300), curve: Curves.ease);
           onPageChanged(index);
         },
         items: [
           BottomNavyBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Feed', style: TextStyle(fontSize: 25, fontFamily: 'Billabong')),
+            icon: const Icon(Icons.home),
+            title: const Text('Feed', style: TextStyle(fontSize: 25, fontFamily: 'Billabong')),
             activeColor: Colors.red,
             inactiveColor: Colors.black,
             textAlign: TextAlign.center,
           ),
           BottomNavyBarItem(
-            icon: Icon(Icons.search),
-            title: Text('Search', style: TextStyle(fontSize: 25, fontFamily: 'Billabong')),
+            icon: const Icon(Icons.search),
+            title: const Text('Search', style: TextStyle(fontSize: 25, fontFamily: 'Billabong')),
             activeColor: Colors.red,
             inactiveColor: Colors.black,
             textAlign: TextAlign.center,
           ),
           BottomNavyBarItem(
-            icon: Icon(Icons.add_a_photo),
-            title: Text('Add', style: TextStyle(fontSize: 25, fontFamily: 'Billabong')),
+            icon: const Icon(Icons.add_a_photo),
+            title: const Text('Add', style: TextStyle(fontSize: 25, fontFamily: 'Billabong')),
             activeColor: Colors.red,
             inactiveColor: Colors.black,
             textAlign: TextAlign.center,
           ),
           BottomNavyBarItem(
-            icon: Icon(Icons.favorite),
-            title: Text('Favorite', style: TextStyle(fontSize: 25, fontFamily: 'Billabong')),
+            icon: const Icon(Icons.favorite),
+            title: const Text('Favorite', style: TextStyle(fontSize: 25, fontFamily: 'Billabong')),
             activeColor: Colors.red,
             inactiveColor: Colors.black,
             textAlign: TextAlign.center,
           ),
           BottomNavyBarItem(
-            icon: Icon(Icons.person),
-            title: Text('Profile', style: TextStyle(fontSize: 25, fontFamily: 'Billabong')),
+            icon: const Icon(Icons.person),
+            title: const Text('Profile', style: TextStyle(fontSize: 25, fontFamily: 'Billabong')),
             activeColor: Colors.red,
             inactiveColor: Colors.black,
             textAlign: TextAlign.center,
