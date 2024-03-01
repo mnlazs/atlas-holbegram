2e931c9c284299ea27468589964b7fe2710b4647
mnlazs <mnl.azs@gmail.com>
2024-02-29 23:23:53 -0600

    updated

---
+++ holbegram/lib/screens/add_picture_screen.dart
@@ -0,0 +1,80 @@
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class AddPicture extends StatefulWidget {
+  final String email;
+  final String password;
+  final String username;
+
+  const AddPicture({
+    Key? key,
+    required this.email,
+    required this.password,
+    required this.username,
+  }) : super(key: key);
+
+  @override
+  _AddPictureState createState() => _AddPictureState();
+}
+
+class _AddPictureState extends State<AddPicture> {
+  Uint8List? _image;
+
+  Future<void> selectImageFromGallery() async {
+    final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      final bytes = await pickedFile.readAsBytes();
+      setState(() {
+        _image = Uint8List.fromList(bytes);
+      });
+    }
+  }
+
+  Future<void> selectImageFromCamera() async {
+    final pickedFile = await ImagePicker().pickImage(source: ImageSource.camera);
+    if (pickedFile != null) {
+      final bytes = await pickedFile.readAsBytes();
+      setState(() {
+        _image = Uint8List.fromList(bytes);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add a Picture'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Muestra la imagen seleccionada o un ícono de usuario por defecto
+            _image != null
+              ? Image.memory(_image!, width: 200, height: 200)
+              : const Icon(Icons.account_circle, size: 200),
+            const SizedBox(height: 20), // Espacio entre imagen y botones
+            // Botones reemplazados por iconos para seleccionar imagen de galería o cámara
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.photo_library),
+                  onPressed: selectImageFromGallery,
+                  tooltip: 'Select from Gallery',
+                ),
+                IconButton(
+                  icon: const Icon(Icons.camera_alt),
+                  onPressed: selectImageFromCamera,
+                  tooltip: 'Take a Picture',
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- holbegram/lib/screens/signup_screen.dart
+++ holbegram/lib/screens/signup_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'login_screen.dart'; // Asegúrate de que la ruta de importación sea correcta
+import 'add_picture_screen.dart'; // Asegúrate de que esta ruta de importación sea correcta
+import 'login_screen.dart'; // No olvides importar login_screen.dart si lo vas a usar
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
-
   @override
   _SignUpScreenState createState() => _SignUpScreenState();
 }
@@ -13,17 +13,16 @@ class _SignUpScreenState extends State<SignUpScreen> {
   final TextEditingController usernameController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController passwordConfirmController = TextEditingController();
-  bool _passwordVisible = true;
+  bool _passwordVisible = false;
 
   @override
   void initState() {
     super.initState();
-    _passwordVisible = true; // Configura la visibilidad inicial de la contraseña
+    _passwordVisible = false;
   }
 
   @override
   void dispose() {
-    // Limpia los controladores cuando el widget sea desmontado
     emailController.dispose();
     usernameController.dispose();
     passwordController.dispose();
@@ -38,7 +37,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            // Aquí puedes agregar otros widgets como el título de la aplicación, imágenes, etc.
+            const SizedBox(height: 20),
             TextField(
               controller: emailController,
               decoration: const InputDecoration(labelText: 'Email'),
@@ -81,22 +80,37 @@ class _SignUpScreenState extends State<SignUpScreen> {
                 ),
               ),
             ),
-            // Botón para enviar el formulario de registro
+            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                // Aquí implementas la lógica para el registro
+                if (passwordController.text == passwordConfirmController.text) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => AddPicture(
+                        email: emailController.text,
+                        password: passwordController.text,
+                        username: usernameController.text,
+                      ),
+                    ),
+                  );
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text("Passwords do not match")),
+                  );
+                }
               },
               child: const Text('Sign Up'),
             ),
             // Botón para navegar de vuelta a la pantalla de inicio de sesión
             TextButton(
               onPressed: () {
-                Navigator.push(
+                Navigator.pushReplacement( // Usamos pushReplacement para reemplazar la pantalla actual en el stack
                   context,
                   MaterialPageRoute(builder: (context) => const LoginScreen()),
                 );
               },
-              child: const Text('Log in'),
+              child: const Text('Already have an account? Log in'),
             ),
           ],
         ),
